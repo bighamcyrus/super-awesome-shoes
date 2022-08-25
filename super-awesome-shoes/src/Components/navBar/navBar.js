52dd4870476801b4db7c089556cc07ad99e4fb73
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-25 10:18:22 -0700

    updated components

--- super-awesome-shoes/src/Components/navBar/navBar.js
+++ super-awesome-shoes/src/Components/navBar/navBar.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-function navBar() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function FormRow() {
   return (
-    <div>
-      HELLO WORLD 
-    </div>
-  )
+    <React.Fragment>
+      <Grid item xs={1}>
+        <Item>Airforce</Item>
+      </Grid>
+      <Grid item xs={1}>
+        <Item>Jordan</Item>
+      </Grid>
+      <Grid item xs={1}>
+        <Item>Yeezy</Item>
+      </Grid>
+      <Grid item xs={1}>
+        <Item>Designer</Item>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
-export default navBar
+export default function NestedGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid container item spacing={4}>
+          <FormRow />
+        </Grid>
+      
+      </Grid>
+    </Box>
+  );
+}
