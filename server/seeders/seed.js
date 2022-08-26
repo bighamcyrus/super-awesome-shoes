4cec7a2138db7e05e29246d92c3f1656b074e00f
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-08-26 13:08:22 -0700

    yes

---
+++ server/seeders/seed.js
@@ -0,0 +1,4 @@
+const db = require("../config/connection");
+const { User } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const thoughtSeeds = require("./thoughtSeeds.json");
