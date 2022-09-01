7ec32ed05da125d79a3965343065247395a2cd8e
Cyrus Bigham <bighamcyrus@gmail.com>
2022-09-01 11:43:32 -0700

    images updated

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -11,196 +11,154 @@ db.once('open', async () => {
         
             {
             category: 'Yeezy',
-            image: '/images/YeezyBlack.png',
+            image: '/images/YeezyBlackR.png',
             title: "Adidas Yeezy Boost 350 V2",
             price: 330
              },
           {
             category: 'Yeezy',
-            image: "/images/YeezySlides.png",
+            image: "/images/YeezySlidesR.png",
             title: "Adidas Yeezy Slides",
             price: 124
           },
           {
             category: 'Yeezy',
-            image: "/images/YeezyFoam.png",
+            image: "/images/YeezyFoamR.png",
             title: "Adidas Yeezy Foam Runner",
             price: 227.00
           },
           {
             category: 'Yeezy',
-            image: "/images/YeezyWave.png",
+            image: "/images/YeezyWaveR.png",
             title: "Adidas Yezzy Boost 700 Wave",
             price: 410.00
           },
           {
             category: 'Yeezy',
-            image: "/images/YeezyZebra.png",
+            image: "/images/YeezyZebraR.png",
             title: "Adidas Yeezy Boost 350 V2 Zebra",
             price: 348.00
           },
           {
-            category: 'airforces',
-            image: "RedOctober.png",
-            title: "Nike Air Yeezy 2 SP Red October",
-            price: 23
-          },
-          {
-            category: 'airforces',
-            image: "/images/JordanRetro.png",
-            title: "Jordan 1 Retro High OG",
-            price: 186.00
-          },
-          {
-            category: 'Yeezy',
-            image: '/images/YeezyBlack.png',
-            title: 'Adidas Yeezy Boost 350 V2',
-            price: 330.00
-          },
-          {
-            category: 'Yeezy',
-            image: '/images/YeezySlides.png',
-            title: 'Adidas Yeezy Slides',
-            price: 124.00
-          },
-          {
-            category: 'Yeezy',
-            image: '/images/YeezyFoam.png',
-            title: 'Adidas Yeezy Foam Runner',
-            price: 227.00
-          },
-          {
-            category: 'Yeezy',
-            image: '/images/YeezyWave.png',
-            title: 'Adidas Yezzy Boost 700 Wave',
-            price: 410.00
-          },
-          {
-            category: 'Yeezy',
-            image: '/images/YeezyZebra.png',
-            title: 'Adidas Yeezy Boost 350 V2 Zebra',
-            price: 348.00
-          },
-          {
             category: 'Jordans',
-            image: '/images/RedOctober.png',
+            image: '/images/RedOctoberR.png',
             title: 'Nike Air Yeezy 2 SP Red October',
             price: 23250.00
           },
           
           {
             category: 'Jordans',
-            image: '/images/JordanRetro.png',
+            image: '/images/JordanRetroR.png',
             title: 'Jordan 1 Retro High OG',
             price: 186.00
           },
           {
             category: 'Jordans',
-            image: '/images/Jordan3.png',
+            image: '/images/Jordan3R.png',
             title: 'Jordan 3 Retro',
             price: 263.00
           },
           {
             category: 'Jordans',
-            image: '/images/Jordan11.png',
+            image: '/images/Jordan11R.png',
             title: 'Jordan 11 Retro',
             price: 266.00
           },
           {
             category: 'Jordans',
-            image: '/images/Jordan4.png',
+            image: '/images/Jordan4R.png',
             title: 'Jordan 4 Retro Red Thunder (PS)',
             price: 110.00
           },
           {
             category: 'Jordans',
-            image: '/images/Jordan11Playoffs.png',
+            image: '/images/Jordan11PlayoffsR.png',
             title: 'Jordan 11 Retro',
             price: 335.00
           },
           {
             category: 'Jordans',
-            image: '/images/Jordan6TravisScott.png',
+            image: '/images/Jordan6TravisScottR.png',
             title: 'Jordan 6 Retro - Travis Scott British Khaki',
             price: 388.00
           },
           
           {
             category: 'Airforce',
-            image: '/images/AirForceWhite.png',
+            image: '/images/AirForceWhiteR.png',
             title: 'Nike Air Force 1 Low 07 White',
             price: 121.00,
           },
           {
             category: 'Airforce',
-            image: '/images/AirForceBlack.png',
+            image: '/images/AirForceBlackR.png',
             title: 'Nike Air Force 1 Low 07 Black',
             price: 115.00
           },
           {
             category: 'Airforce',
-            image: '/images/AFMid.png',
+            image: '/images/AFMidR.png',
             title: 'Nike Air Force 1 Mid',
             price: 110.00
           },
           {
             category: 'Airforce',
-            image: '/images/AFMidNH.png',
+            image: '/images/AFMidNHR.png',
             title: 'Nike Air Force 1 Mid NH',
             price: 134.00
           },
           {
             category: 'Airforce',
-            image: '/images/AFLowShadow.png',
+            image: '/images/AFLowShadowR.png',
             title: 'Nike Air Force 1 Low Shadow',
             price: 121.00
           },
           {
             category: 'Airforce',
-            image: '/images/AFHigh.png',
+            image: '/images/AFHighR.png',
             title: 'Nike Air Force 1 High',
             price: 177.00
           },
           
           {
             category: 'Designer',
-            image: '/images/BalTrack.png',
+            image: '/images/BalTrackR.png',
             title: 'Balenciaga Track',
             price: 782.00
           },
           {
             category: 'Designer',
-            image: '/images/Gucci.png',
+            image: '/images/GucciR.png',
             title: 'Gucci Rhyton - Vintage Logo',
             price: 675.00
           },
           {
             category: 'Designer',
-            image: '/images/BalSpeed.png',
+            image: '/images/BalSpeedR.png',
             title: 'Balenciaga Speed Graffiti Trainers',
             price: 600.00
           },
           {
             category: 'Designer',
-            image: '/images/Dior.png',
+            image: '/images/DiorR.png',
             title: 'Dior B22',
             price: 1262.00
           },
           {
             category: 'Designer',
-            image: '/images/LV.png',
+            image: '/images/LVR.png',
             title: 'Louis Vuitton Trainer Sneaker Boot',
             price: 2522.00
           },
           {
             category: 'Designer',
-            image: '/images/OffWhite.png',
+            image: '/images/OffWhiteR.png',
             title: 'OFF-WHITE Vulc Low',
             price: 256.00
           },
           {
             category: 'Designer',
-            image: '/images/McQueen.png',
+            image: '/images/McQueenR.png',
             title: 'Alexander McQueen Tread Slick Lace Up Boot',
             price: 512.00
           },
